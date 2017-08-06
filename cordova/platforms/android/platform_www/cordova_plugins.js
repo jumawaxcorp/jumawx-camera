25b033f6d3d3d2e4ccd99e84fb936dbe04965828
obus <irfanhakimobuz@gmail.com>
2017-08-06 13:49:37 +0700

    dfsd
    fsdfsdf

---
+++ cordova/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,53 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-camera.Camera",
+        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "id": "cordova-plugin-camera.CameraPopoverOptions",
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "id": "cordova-plugin-camera.camera",
+        "file": "plugins/cordova-plugin-camera/www/Camera.js",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "id": "cordova-plugin-camera.CameraPopoverHandle",
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
+    },
+    {
+        "id": "cordova-plugin-ftp.ftp",
+        "file": "plugins/cordova-plugin-ftp/www/ftp.js",
+        "pluginId": "cordova-plugin-ftp",
+        "clobbers": [
+            "cordova.plugin.ftp"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.2",
+    "cordova-plugin-compat": "1.1.0",
+    "cordova-plugin-camera": "2.4.1",
+    "cordova-plugin-ftp": "1.0.3"
+};
+// BOTTOM OF METADATA
+});
